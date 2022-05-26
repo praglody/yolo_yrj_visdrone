6aaed1848e5237507097fa833fe2cc7cd9179b74
zhudp <praglody@163.com>
2022-05-26 14:12:23 +0800

    ignore

---
+++ visdrone2yolov5.py
@@ -0,0 +1,35 @@
+from utils.general import download, os, Path
+from PIL import Image
+from tqdm import tqdm
+
+
+def convert_box(size, box):
+    # Convert VisDrone box to YOLO xywh box
+    dw = 1. / size[0]
+    dh = 1. / size[1]
+    return (box[0] + box[2] / 2) * dw, (box[1] + box[3] / 2) * dh, box[2] * dw, box[3] * dh
+
+
+def visdrone2yolo(dir):
+    dir = Path(dir)
+    (dir / 'labels').mkdir(parents=True, exist_ok=True)  # make labels directory
+    pbar = tqdm((dir / 'annotations').glob('*.txt'), desc=f'Converting {dir}')
+    for f in pbar:
+        img_size = Image.open((dir / 'images' / f.name).with_suffix('.jpg')).size
+        lines = []
+        with open(f, 'r') as file:  # read annotation.txt
+            for row in [x.split(',') for x in file.read().strip().splitlines()]:
+                if row[4] == '0':  # VisDrone 'ignored regions' class 0
+                    continue
+                cls = int(row[5]) - 1
+                box = convert_box(img_size, tuple(map(int, row[:4])))
+                lines.append(f"{cls} {' '.join(f'{x:.6f}' for x in box)}\n")
+                with open(str(f).replace(os.sep + 'annotations' + os.sep, os.sep + 'labels' + os.sep), 'w') as fl:
+                    fl.writelines(lines)  # write label.txt
+
+
+root = os.path.dirname(os.path.realpath(__file__))
+dir = root + os.sep + "dataset" + os.sep + "VisDrone"
+for fold in os.listdir(dir):
+    if fold[0:8] == "VisDrone":
+        visdrone2yolo(dir + os.sep + fold)
